2e39b38bf33840f039b1622825579f16efea8c7e
SandunC000 <sanduncooray000@gmail.com>
2023-09-11 19:03:59 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,49 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container } from 'react-bootstrap'
+import { Routes, Route, Navigate } from "react-router-dom"
+import { NewNote } from './NewNote'
+
+export type Note = {
+  id: string
+} & NoteData
+
+export type RawNote = {
+  id: string
+}
+
+export type RawNoteData = {
+  id: string
+  markdown: string
+  tagIds: string[]
+}
+
+export type NoteData = {
+  title: string
+  markdown: string
+  tags: Tag[]
+}
+
+export type Tag = {
+  id : string
+  label: string
+} 
+
+export function App() {
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES",[])
+  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS",[])
+  return (
+    <Container className='my-4'>
+      <Routes>
+        <Route path='/' element={<h1>Home</h1>}></Route>
+        <Route path='/new' element={<NewNote />}></Route>
+        <Route path='/:id'>
+          <Route index element={<h1>Show</h1>} />
+          <Route path='edit' element={<h1>Edit</h1>} />
+
+        </Route>
+        <Route path='*' element={<Navigate to={"/"} />} />
+      </Routes>
+    </Container>
+  )
+}
+
